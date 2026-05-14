7b7680874118c57a3fd9df6232e4f838c38aa7a0
初音ミク <animegirl@noreply.codeberg.org>
2026-05-14 11:47:08 +0200

    update to latest

--- flake.nix
+++ flake.nix
@@ -4,9 +4,9 @@
     inputs =
     {
         # https://github.com/NixOS/nixpkgs/tree/nixos-25.11
-        nixpkgs.url = "github:nixos/nixpkgs/a4bf06618f0b5ee50f14ed8f0da77d34ecc19160";
+        nixpkgs.url = "github:nixos/nixpkgs/8fd9daa3db09ced9700431c5b7ad0e8ba199b575";
         # https://github.com/NixOS/nixpkgs (master)
-        nixpkgs-master.url = "github:nixos/nixpkgs/63049db4c80c938eed39fc750aaa4c0ef41d16c0";
+        nixpkgs-master.url = "github:nixos/nixpkgs/9ec84f9983acabdf3e558f0e7f510cdbb34995b4";
     };
 
     outputs = { nixpkgs, nixpkgs-master, ... }:
