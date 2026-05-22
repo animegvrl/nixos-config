3045c993224fd4bc373ddd9f394e6209e90099e6
初音ミク <animegirl@noreply.codeberg.org>
2026-05-22 21:59:52 +0200

    explicitly enable firewall

--- dendrites/networking.nix
+++ dendrites/networking.nix
@@ -19,9 +19,7 @@
     # networking.proxy.default = "http://user:password@proxy:port/";
     # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
-    # Open ports in the firewall.
+    networking.firewall.enable = true;
     networking.firewall.allowedTCPPorts = [ 80 ];
     networking.firewall.allowedUDPPorts = [ 80 ];
-    # Or disable the firewall altogether.
-    # networking.firewall.enable = false;
 }
